7b17d575af39e57d39a369c7d9ac61829274f0d0
NotaKron <sergsamof@gmail.com>
2020-10-27 14:27:43 +0300

    Add authentication and redirect on main page

--- mybh/mybh/settings.py
+++ mybh/mybh/settings.py
@@ -45,13 +45,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainmenu.apps.MainmenuConfig',
 ]
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- mybh/mybh/urls.py
+++ mybh/mybh/urls.py
@@ -18,9 +18,13 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.conf.urls import url, include
+from mainmenu import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.main_menu),
+    url(r'^login/', views.authorization)
 ]
 
 """if settings.DEBUG:
